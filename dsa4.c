6815de9c0b0d29ff625b88b952f0c61a63239a8c
ROHAN YADAV <rohanyadav1832007@gmail.com>
2026-02-18 15:41:02 +0530

    Add files via upload

---
+++ dsa4.c
@@ -0,0 +1,26 @@
+//wap to reverse an array in place
+#include <stdio.h>
+void reverseArray(int arr[], int n) {
+    int start = 0;
+    int end = n - 1;
+    while (start < end) {
+        // Swap arr[start] and arr[end]
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    reverseArray(arr, n);
+
+    printf("Reversed array: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
